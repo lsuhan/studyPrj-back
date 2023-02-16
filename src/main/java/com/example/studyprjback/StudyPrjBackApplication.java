9113c7e881af6bb8a31fd688777d4fe576e9eabf
lsuhan00 <lsuhan00@hanmail.net>
2023-02-16 10:36:00 +0900

    Initial commit

---
+++ src/main/java/com/example/studyprjback/StudyPrjBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyprjback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPrjBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyPrjBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyprjback/StudyPrjBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyprjback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPrjBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
